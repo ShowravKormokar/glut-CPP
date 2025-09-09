f39b48de75d4e12dd43038d31ef52932bc6639fb
ShowravKormokar <showravkormokar354@gmail.com>
2025-09-09 10:45:13 +0600

    Program for draw vertical line

---
+++ verticalLine.cpp
@@ -0,0 +1,64 @@
+/*The screen coordinates of the points on a Vertical
+Line are obtained by keeping the value of x constant and repeatedly incrementing the y
+value by one unit.*/
+#include <bits/stdc++.h>
+#include <GL/glut.h>
+#include <windows.h>
+using namespace std;
+
+// Vertical Line Algo
+void VL(int x0, int y0, int x1, int y1)
+{
+    if (y0 <= y1)
+    {
+        int x = x0;
+        int y = y0;
+
+        while (y <= y1)
+        {
+            glBegin(GL_POINTS);
+            glVertex2i(x, y);
+            glEnd();
+            y += 1;
+        }
+    }
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT); // Clear the screen
+    glColor3f(1, 1, 1);
+
+    glBegin(GL_LINES);
+    glVertex2f(0.0, 500.0);
+    glVertex2f(0.0, -500.0);
+    glVertex2f(500.0, 0.0);
+    glVertex2f(-500.0, 0.0);
+    glEnd();
+
+    glPointSize(4);
+
+    // Vertical Line
+    VL(100, 100, 100, 300);
+
+    glFlush();
+}
+
+// Main function
+int main(int argc, char **argv)
+{
+    glutInit(&argc, argv);                       // Initialize GLUT
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); // Set display mode
+    glutInitWindowSize(500, 500);                // Set window size
+    glutInitWindowPosition(100, 100);            // Set window position
+    glutCreateWindow("DDA");                     // Create window with title
+
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // Set background color to black
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(-500.0, 500.0, -500.0, 500.0); // Set coordinate system
+
+    glutDisplayFunc(display); // Register display callback
+    glutMainLoop();           // Enter the GLUT event loop
+
+    return 0;
+}
