380912fd6f1dcfa401b246af1f743520d9b6296b
ShowravKormokar <showravkormokar354@gmail.com>
2025-11-18 10:38:07 +0600

    Modify collision logic

--- Jet-Game/include/checkCollision.h
+++ Jet-Game/include/checkCollision.h
@@ -5,10 +5,18 @@
 using namespace std;
 
 //--------- Collision Detection Function
-bool checkCollision(float x1, float y1, float w1, float h1, float x2, float y2, float w2, float h2)
+bool checkCollision(float x1, float y1, float w1, float h1,
+                    float x2, float y2, float w2, float h2)
 {
-    return (x1 < x2 + w2 &&
-            x1 + w1 > x2 &&
-            y1 < y2 + h2 &&
-            y1 + h1 > y2);
+    // No collision cases:
+    if (x1 + w1 < x2)
+        return false; // Jet right < Block left
+    if (x1 > x2 + w2)
+        return false; // Jet left > Block right
+    if (y1 + h1 < y2)
+        return false; // Jet top < Block bottom
+    if (y1 > y2 + h2)
+        return false; // Jet bottom > Block top
+
+    return true; // Otherwise collision
 }
